7f27defc2fcbf1eea6eef9c207625fe827f8e942
Aashish909 <aashish24062000@gmail.com>
2025-05-23 21:10:53 +0530

    update eslinit.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add this block for custom rules
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // or "off"
+    },
+  },
 ];
 
 export default eslintConfig;
